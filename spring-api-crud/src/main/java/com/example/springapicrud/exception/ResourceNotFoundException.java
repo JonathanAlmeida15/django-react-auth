1b0c36fe4c9ffb1a7fb8414c8dc24a752e7bcdf6
Jonathan Gabriel de Carvalho Almeida <63514737+JonathanAlmeida15@users.noreply.github.com>
2025-11-27 03:30:59 -0300

    Deletando um projeto que subiu por erro no Git Push

--- spring-api-crud/src/main/java/com/example/springapicrud/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapicrud.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
